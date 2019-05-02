a13a9098e1e094727f7ac9df0707558d54915f7f
nimrod-vanir <nimrodvanir97@gmail.com>
2019-05-02 18:29:02 -0400

    Update client2.py

--- client2.py
+++ client2.py
@@ -3,6 +3,7 @@ import asyncio
 import sys
 import random
 import asyncio
+import time
 
 import aiocoap.resource as resource
 from aiocoap import *
@@ -17,25 +18,33 @@ async def main():
 
     context = await Context.create_client_context()
 
-    if(sys.argv[1] == "GET"):
-        request = Message(code=GET, payload=sys.argv[2].encode('UTF-8'), uri='coap://localhost/.well-known/core')
-        try:
+    if random.uniform(0, 1) < 0.98:
+
+        start = time.time()
+
+        if(sys.argv[1] == "GET"):
+            await asyncio.sleep(random.uniform(0, 2))
+
+            request = Message(code=GET, payload=sys.argv[2].encode('UTF-8'), uri='coap://localhost/.well-known/core')
+            try:
+                response = await context.request(request).response
+            except Exception as e:
+                print('Failed to fetch resource:')
+                print(e)
+            else:
+                print('Result: %s\n%r'%(response.code, response.payload))
+
+        elif(sys.argv[1] == "PUT" and sys.argv[2]):
+            await asyncio.sleep(random.uniform(0, 2))
+
+            payload = sys.argv[2].encode('UTF-8')
+            request = Message(code=PUT, payload=payload, uri="coap://localhost/.well-known/core")
+
             response = await context.request(request).response
-        except Exception as e:
-            print('Failed to fetch resource:')
-            print(e)
-        else:
+
             print('Result: %s\n%r'%(response.code, response.payload))
 
-    elif(sys.argv[1] == "PUT" and sys.argv[2]):
-        await asyncio.sleep(random.uniform(0, 2))
-
-        payload = sys.argv[2].encode('UTF-8')
-        request = Message(code=PUT, payload=payload, uri="coap://localhost/.well-known/core")
-
-        response = await context.request(request).response
-
-        print('Result: %s\n%r'%(response.code, response.payload))
-
+        #print("RTT: " + str(time.time() - start) + " secs")
+        return time.time() - start
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
